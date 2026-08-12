a262c65d8bba84b8466bccead4c84e6f95e2c21e
𝓐𝓷𝓪𝓛𝔂  ୨୧ <analygiaptc@gmail.com>
2026-08-12 15:50:51 -0300

    Update Cliente.php

--- TRABALHO_LABPROGRAMACAO/Cliente.php
+++ TRABALHO_LABPROGRAMACAO/Cliente.php
@@ -1,68 +1,41 @@
 <?php
-
 require_once "Pessoa.php";
-
 class Cliente extends Pessoa
 {
     private Data $dataCadastro;
     private string $preferencias;
-
-    public function __construct(
-        string $nome,
-        string $cpf,
-        string $sexo,
-        Data $dataNascimento
-    ) {
-        parent::__construct($nome, $cpf, $sexo, $dataNascimento);
-
+    public function __construct(string $nome, string $cpf, string $sexo, Data $data)
+    {
+        parent::__construct($nome, $cpf, $sexo, $data);
         $this->dataCadastro = new Data(date("d"), date("m"), date("Y"));
-
         $this->preferencias = "";
     }
-
-    public function imprimir(){
-
-    }
-
-
-    
-    /**
-     * Get the value of dataCadastro
-     */ 
     public function getDataCadastro()
     {
         return $this->dataCadastro;
     }
-
-    /**
-     * Set the value of dataCadastro
-     *
-     * @return  self
-     */ 
-    public function setDataCadastro(Data $dataCadastro)
+    public function setDataCadastro($dataCadastro)
     {
         $this->dataCadastro = $dataCadastro;
-
         return $this;
     }
-
-    /**
-     * Get the value of preferencias
-     */ 
     public function getPreferencias()
     {
         return $this->preferencias;
     }
-
-    /**
-     * Set the value of preferencias
-     *
-     * @return  self
-     */ 
-    public function setPreferencias(string $preferencias)
+    public function setPreferencias($preferencias)
     {
         $this->preferencias = $preferencias;
-
         return $this;
     }
+    public function imprimir()
+    {
+        echo $this->getNome() . "<br>";
+        echo $this->getCpf() . "<br>";
+        echo $this->getSexo() . "<br>";
+        echo $this->getDataNasc() . "<br>";
+        echo $this->getDataCadastro() . "<br>";
+        echo $this->getPreferencias() . "<br>";
+    }
 }
+?>
